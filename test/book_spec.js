2b38ae590aad3fb202b77a4f0ab1da77c6e74bc3
Ivan Storck <ivanoats@gmail.com>
2014-02-07 17:08:23 -0800

    converting tabs to spaces

--- test/book_spec.js
+++ test/book_spec.js
@@ -1,19 +1,19 @@
 var expect = require('chai').expect,
-	Book = require('../lib/book').Book;
+  Book = require('../lib/book').Book;
 
 describe('Book object tests', function(){
-	var book;
+  var book;
 
-	beforeEach(function(){
-		book = new Book("Addy Osmani","Learning JavaScript Design Patterns",2013);
-	})
+  beforeEach(function(){
+    book = new Book("Addy Osmani","Learning JavaScript Design Patterns",2013);
+  })
 
-	describe('constructor', function() {
-		it('book should be truthy (exists)', function(){
-			expect(book).to.be.ok;
-		});
-		it('book should have author property', function() {
-			expect(book).to.have.property('author');
-		})
-	})
-})	
+  describe('constructor', function() {
+    it('book should be truthy (exists)', function(){
+      expect(book).to.be.ok;
+    });
+    it('book should have author property', function() {
+      expect(book).to.have.property('author');
+    })
+  })
+})
