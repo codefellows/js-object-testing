f94dfa03fbf2a1facd8fb113bcb92a9b54fc2f60
Ivan Storck <ivanoats@gmail.com>
2014-02-14 15:43:37 -0800

    implemented catalog updated jshintrc to ignore some more chai globals fixed chai and jshint issues in catalog spec

--- test/catalog_spec.js
+++ test/catalog_spec.js
@@ -1,5 +1,6 @@
 var expect = require('chai').expect,
-  Catalog = require('../lib/catalog').Catalog;
+  Catalog = require('../lib/catalog').Catalog,
+  Book = require('../lib/book').Book;
 
 describe('Catalogs', function() {
   'use strict';
@@ -11,22 +12,23 @@ describe('Catalogs', function() {
 
   it('can add a book', function() {
     catalog.add(new Book('George R. R. Martin','A Game of Thrones', 1997));
-    expect(catalog.includes('A Game of Thrones')).to.be.true;
+    expect(catalog.includes('A Game of Thrones')).to.be.true; // jshint ignore: line
   });
 
   it('are initially empty', function() {
-    expect(catalog.size()).to.be(0);
-  })
+    expect(catalog.size()).to.equal(0);
+  });
 
   it('have a count of Books called size', function() {
     catalog.add(new Book('George R. R. Martin','A Game of Thrones', 1997));
-    expect(catalog.size()).to.be(1);
-  })
+    expect(catalog.size()).to.equal(1);
+  });
 
   it('can check the inclusion of a book', function() {
-    expect(catalog.includes('A Game of Thrones')).to.be.false;
-  })
+    // #TODO figure out syntatic sugar validation in jshint - the false true value thing
+    expect(catalog.includes('A Game of Thrones')).to.be.false; // jshint ignore: line
+  });
 
-  it('should know the total number of copies of a particular book it has')
+  it('should know the total number of copies of a particular book it has');
 
 });
