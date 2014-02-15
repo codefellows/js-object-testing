d1c99586ef5bbab4568990a3d4635725631937ff
Ivan Storck <ivanoats@gmail.com>
2014-02-14 16:15:52 -0800

    change circulation so it was using association instead of parameters. changed the signature of checkOut to take out the catalog. changed the parameters of checkOut to take an object literal added an expectation that checkOut now returns true

--- test/circulation_spec.js
+++ test/circulation_spec.js
@@ -9,8 +9,8 @@ describe('Circulation', function() {
   var circulation, catalog;
 
   beforeEach(function(){
-    circulation = new Circulation();
     catalog = new Catalog();
+    circulation = new Circulation(catalog);
   });
 
   describe('keeps track of books that are checked out', function() {
@@ -18,7 +18,9 @@ describe('Circulation', function() {
       var book = new Book('Addy Osmani','Learning JavaScript Design Patterns',2013);
       var patron = new Patron('Oscar Peterson');
       catalog.add(book);
-      circulation.checkOut(catalog, book, patron);
+      expect(
+        circulation.checkOut({book: book, patron: patron})
+        ).to.be.true; //jshint ignore:line
       expect(circulation.numberOfBooksOut()).to.equal(1);
     });
     it('can return books');
